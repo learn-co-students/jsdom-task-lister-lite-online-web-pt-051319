4e2fdf929e17def76f576b9a63cc31cc86225136
Jason McGloin <jason.mcgloin@gmail.com>
2019-11-13 20:30:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  let form = document.getElementById('create-task-form');
+  let newTask = document.getElementById('new-task-description');
+  let tasksList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+  	e.preventDefault();
+  	let newListItem = document.createElement('li');
+  	newListItem.textContent = newTask.value;
+  	newTask.value = "";
+  	tasksList.appendChild(newListItem)
+  });
 });
+
+
+
+// <form id="create-task-form" action="#" method="post">
+//       <label for="new-task-description">Task description:</label>
+//       <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+//       <input type="submit" value="Create New Task">
+//     </form>
+
+//     <div id="list">
+//       <h2>My Todos</h2>
+//       <ul id="tasks">
