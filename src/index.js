25ac99874aba6538758e67b59f96a426f0181b93
jamievullo <jvullo@protonmail.com>
2019-11-15 09:36:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+      let x = document.getElementById("create-task-form");
+      let description = document.getElementById("new-task-description");
+      let z = document.getElementById("tasks");
+
+
+
+      // console.log(x)
+      x.addEventListener("submit", createTask);
+      // z.addEventListener("click", deleteTask);
 });
+
+let createTask = event => {
+  event.preventDefault();
+
+  let description = document.getElementById("new-task-description")
+  let newTask = document.createElement("li")
+
+  newTask.innerHTML = description.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+// let deleteTask = e => {
+//   e.preventDefault();
+
+//   let z = document.getElementById("tasks");
+//   z.parentNode.removeChild(task);
+
+  
+// };
