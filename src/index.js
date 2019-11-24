7761a5b6c03b7b94c8b741b33615f11953b17db2
KristinHannah <kristinhannahk@gmail.com>
2019-11-24 14:26:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
+  taskForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const newTask = document.getElementById('new-task-description').value;
+    if (newTask) {
+      const ul = document.getElementById('tasks');
+      const li = document.createElement("li");
+      const button = document.createElement("button");
+      li.innerText = newTask;
+      button.innerText = "X";
+      button.id = newTask;
+      li.appendChild(button);
+      ul.appendChild(li);
+      event.target.reset()
+    }
+  });
 });
