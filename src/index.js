cb5ae26b95752b06f0a348a34ed0971878ee3084
Jess Rezac <jess.rezac@gmail.com>
2019-11-29 14:42:31 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
+
+  document.querySelector('#create-task-form').addEventListener('submit', function(e){
+     e.preventDefault();    //stop form from submitting
+    let taskDescription = document.getElementById('new-task-description').value;
+    let tasks = document.getElementById('tasks');
+    let newTask = document.createElement('li');
+    newTask.innerText = taskDescription
+    tasks.appendChild(newTask)
+  });
 });
