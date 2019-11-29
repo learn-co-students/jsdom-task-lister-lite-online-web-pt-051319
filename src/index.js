5942689ff5c129f57bba133465aa15138ea602c5
Jess Rezac <jess.rezac@gmail.com>
2019-11-29 14:47:30 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // your code here
 
   document.querySelector('#create-task-form').addEventListener('submit', function(e){
-     e.preventDefault();    //stop form from submitting
+    e.preventDefault();    //stop form from submitting
     let taskDescription = document.getElementById('new-task-description').value;
     let tasks = document.getElementById('tasks');
     let newTask = document.createElement('li');
