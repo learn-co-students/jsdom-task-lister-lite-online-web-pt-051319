72371655218052e0d795e6a55c470145fdfa26d8
jamievullo <jvullo@protonmail.com>
2019-12-03 11:17:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-      let x = document.getElementById("create-task-form");
-      let description = document.getElementById("new-task-description");
-      let z = document.getElementById("tasks");
 
+  const form = document.getElementById("create-task-form")
 
+  form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    debugger;
+    const x = event.target["0"].value
+    const box = document.getElementById("tasks")
+    const element = document.createElement('li')
+    box.appendChild(element)
+    element.innerHTML = x
+  })
 
-      // console.log(x)
-      x.addEventListener("submit", createTask);
-      // z.addEventListener("click", deleteTask);
 });
-
-
-
-let createTask = event => {
-  event.preventDefault();
-
-  let description = document.getElementById("new-task-description")
-  let newTask = document.createElement("li")
-
-  newTask.innerHTML = description.value;
-
-  appendNewTask(newTask);
-  event.target.reset();
-};
-
-
- const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
-
-// let deleteTask = e => {
-//   e.preventDefault();
-
-//   let z = document.getElementById("tasks");
-//   z.parentNode.removeChild(task);
-
-  
-// };
