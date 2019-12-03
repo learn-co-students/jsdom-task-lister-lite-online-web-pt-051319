6bd22877677b38006ce60b8af77ee6b2a8e5742d
jamievullo <jvullo@protonmail.com>
2019-12-03 17:06:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // setting a variable to the form where I hope to grab the input of that form.
   const form = document.getElementById("create-task-form")
   // adding an event listener on the variable of the form then "preventing the default" 
-  // behavior of the submit button. 
+  // behavior of the submit button.
   form.addEventListener("submit", function(event) {
     
     event.preventDefault()
@@ -11,11 +11,53 @@ document.addEventListener("DOMContentLoaded", () => {
     // setting a variable to where I want the "input" to go
     const box = document.getElementById("tasks")
     // setting a variable to create an element ("li") in the "box"(ul) where my input is to go
-    const element = document.createElement('li')
+    const element = document.createElement("li")
     // appendChild method to add the created li because JS is a mindfuck and thats just the way it is
-    box.appendChild(element)
-    // place the inputted value into the created li.
-    element.innerHTML = listItem
-  })
+    const button = document.createElement("button")
+    const editButton = document.createElement("button")
 
+    button.id = "delete"
+    editButton.id = "edit"
+    
+    editButton.innerHTML = "Edit"
+    button.innerHTML = "Delete"
+    element.innerHTML = listItem
+
+    box.appendChild(element)
+    element.appendChild(button)
+    element.appendChild(editButton)
+
+    deleteTask();
+    editTask();
+  })
+  
 });
+
+function deleteTask() {
+  const allTasks = document.querySelectorAll("button#delete")
+  allTasks.forEach(task => {
+    task.addEventListener("click", function(e) {
+      
+      task.parentNode.remove()
+    })
+  })
+}
+
+function editTask() {
+  const allTasks = document.querySelectorAll("button#edit")
+  allTasks.forEach(task => {
+    task.addEventListener("click", function(e) {
+    const newTask = task.parentElement.firstChild
+     
+    const textBox = document.createElement("textarea")
+    textBox.innerText = `${newTask}`
+    
+    task.parentNode.appendChild(textBox)
+    textBox.addEventListener("keydown", function(e) {
+      const keypressed = e.key
+      if keypressed === "Enter"
+      console.log("keypress")
+     })
+    })
+  })
+}
