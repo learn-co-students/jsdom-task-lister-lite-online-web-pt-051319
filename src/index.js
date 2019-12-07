fbcbb1c16de17924e35478f91971d223668e8195
Haj B <phajib@gmail.com>
2019-12-07 03:18:40 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.getElementById('create-task-form');
+  
+  taskForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let newTask = document.getElementById('new-task-description').value;
+   
+    if (newTask) {
+      let ul = document.getElementById('tasks');
+      let li = document.createElement("li");
+      let editInput = document.createElement("input");
+      let editButton = document.createElement("button");
+      let deleteButton = document.createElement("button");
+      
+      li.innerText = newTask;
+      editInput.type = "text";
+
+      editButton.innerText = "Edit";
+      editButton.className = "edit";
+      deleteButton.innerText = "X";
+      deleteButton.className = "delete";
+      
+      // li.appendChild(editInput);
+      li.appendChild(editButton);
+      li.appendChild(deleteButton);
+
+      ul.appendChild(li);
+      event.target.reset()
+
+      let edit = document.querySelector("button.edit");
+
+      li.onclick = function() {this.parentNode.removeChild(this);}
+      // edit.onclick = editTask;
+      // document.querySelector("button.edit").onclick = editTask;
+    }
+  })
 });
+
+let editTask = function() {
+  let listItem = this.parentNode;
+  
+  let editInput = listItem.querySelector('input[type=text]');
+  let label = listItem.querySelector("label");
+  let containsClass = listItem.classList.contains("editMode");
+      
+      if(containsClass){
+        label.innerText = editInput.value;
+      } else {
+        editInput.value = label.innerText;
+      }
+
+      listItem.classList.toggle("editMode");
+}
