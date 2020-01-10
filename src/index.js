6ca8a8deb395260b02dae073be66f0e3cae1dc5f
Michael Mottola <mottola64@gmail.com>
2020-01-10 12:44:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  form.addEventListener( 'submit', function(e){ 
+    e.preventDefault()
+    let v = event.target["0"].value
+    const item = document.createElement('li')
+    item.innerText = v
+    document.getElementById('tasks').append(item)
+    event.target.reset()
+  } )
 });
