58d2f1b3e300abce6f420ecfd9c1b87a13aff2b3
ablakerose <bdecktor@gmail.com>
2020-05-29 16:49:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let input = document.querySelector("#new-task-description");
+    if (input.value !== "") {
+      appendList(input.value);
+      input.value = "";
+    }
+  });
+
+  function appendList(text) {
+    const li = document.createElement("li");
+    li.innerText = text;
+    list.append(li);
+  }
 });
