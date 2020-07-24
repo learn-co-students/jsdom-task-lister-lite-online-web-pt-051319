5e90f450f019c905673395973cbb2b7ea6c0eae7
ashok997 <ashok997@gmail.com>
2020-07-24 12:31:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", addToList);
 });
+
+function addToList() {
+  event.preventDefault();
+  let addedTask = document.getElementById("new-task-description").value;
+
+  let task = document.getElementById("list");
+  let node = document.createElement("li");
+  let textnode = document.createTextNode(addedTask);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "X";
+  deleteButton.addEventListener("click", deleteTask);
+
+  node.appendChild(textnode);
+  task.appendChild(node);
+  node.appendChild(deleteButton);
+  event.target.reset();
+}
+
+deleteTask = (e) => {
+  e.preventDefault();
+  node = e.target.parentNode;
+  node.remove();
+};
